5c4601376f5bc6a41ff1f1fb20e378f45cce6ea1
greforce <greforce76@gmail.com>
2019-01-06 15:13:49 +0300

    Add AppLayout

--- src/App/index.js
+++ src/App/index.js
@@ -1,31 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
-import styled, { css } from 'styled-components';
-
-const MyButton = styled.button`
-  color: green;
-  ${props => props.primary && css`
-    background: white;
-    color: palevioletred;
-  `}
-`;
-
-const TomatoButton = styled(MyButton)`
-  color: tomato;
-  border-color: tomato;
-`;
+import AppLayout from './AppLayout';
 
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <AppLayout>
         <WelcomeMessage />
-        <MyButton>Hello</MyButton>
-        <MyButton primary>Hello</MyButton>
-        <TomatoButton primary>Hello</TomatoButton>
-      </div>
+      </AppLayout>
     );
   }
 }
